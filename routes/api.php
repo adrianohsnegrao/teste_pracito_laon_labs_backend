89df6a7aae3947b6c5a4cc4d58697aa41a4e9838
Adriano Negrão <adrianohsn2022@gmail.com>
2025-01-18 13:57:30 -0400

    adicionando rotas e configurando controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PlanController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('profiles', ProfileController::class);
+    Route::resource('plans', PlanController::class);
+    Route::resource('series', SeriesController::class);
+    Route::resource('movies', MovieController::class);
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
