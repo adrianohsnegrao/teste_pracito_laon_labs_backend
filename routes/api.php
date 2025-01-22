91bfc9689eda5677803ad99e0873aa9460375581
Adriano Negrão <adrianohsn2022@gmail.com>
2025-01-21 21:22:42 -0400

    Ajusta rotas de login

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('users', UserController::class);
@@ -30,6 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::resource('plans', PlanController::class);
     Route::resource('series', SeriesController::class);
     Route::resource('movies', MovieController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::post('login', [AuthController::class, 'login']);
