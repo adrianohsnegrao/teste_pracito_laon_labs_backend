984be9972bc92b5477a4a2b5bd0fb0ca03987d90
Adriano Negrão <adrianohsn2022@gmail.com>
2025-01-23 13:48:13 -0400

    Ajusta nivel de acesso no user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('profile_level_id')->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
